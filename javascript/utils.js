15e7bdc1a2c1f90b7def359fe2765183a1d6885d
crocodeev <svin862003@mail.ru>
2019-10-27 17:10:32 +0300

    add some check to prevent some user actions

---
+++ javascript/utils.js
@@ -0,0 +1,24 @@
+'use strict'
+
+exports.isPathFilled = function (path) {
+  if (path === undefined) {
+      return false;
+  }
+  return true;
+}
+
+exports.onlyFilledForms = function(forms){
+
+  let pathArray = [];
+
+  for (var i = 0; i < 3; i++) {
+    if (forms[i].value !== "") pathArray.push(forms[i].value);
+  }
+  return pathArray;
+}
+
+exports.isArrayFilled = function (arr) {
+  let result;
+  arr.length > 0 ? result = true : result = false;
+  return result;
+}
